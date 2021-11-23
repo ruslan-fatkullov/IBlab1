c187b7cd9bdfce127325bfe6f7a67400a888e9c2
ruslan-fatkullov <fatkullov1999@gmail.com>
2021-11-23 15:57:46 +0400

    code and decode success

--- four_lab.py
+++ four_lab.py
@@ -1,38 +1,86 @@
 import random
 
 
-def crypt(message, key):
-    n = len(message)
-    d = n % 6
+def crypt_message(message, m, n):
+    string_len = len(message)
+    d = string_len % 6
     add = []
     if d != 0:
         add = [chr(random.randrange(ord('a'), ord('z'), 1)) for _ in range(6 - d)]
     message += ''.join(add)
     result_string = ""
-    for i in range(0, n, 6):
-        string = [message[i+j] for j in range(6)]
+    for i in range(0, string_len, 6):
+        string = [message[i + j] for j in range(6)]
         substring = ""
-        for j in range(3):
-            substring += string[key[j]*2]
-            substring += string[key[j]*2+1]
+        for j in range(n, 6):
+            substring += string[j]
         result_string += substring
+        substring = ""
+        for j in range(0, m):
+            substring += string[j]
+        result_string += substring
+        substring = ""
+        for j in range(m, n):
+            substring += string[j]
+        result_string += substring
+
     return result_string
 
 
-def decrypt(code):
-    n = len(code)
-    for i in range(0, n, 6):
-        substring = []
+def decrypt_message(code, m, n):
+    string_len = len(code)
+    decode_string = ""
+    for i in range(0, string_len, 6):
+        string = [code[i + j] for j in range(6)]
+        for j in range(n, 6):
+            decode_string += string[j]
+        for j in range(0, m):
+            decode_string += string[j]
+        for j in range(m, n):
+            decode_string += string[j]
+    return decode_string
 
 
-def bin2dec():
-    pass
+def crypt(iteration_count, message, m, n):
+    res = message
+    # print(m)
+    # print(n)
+    for i in range(iteration_count):
+        res = crypt_message(res, m[i], n[i])
+        # print(str(i+1) + " " + res)
+    return res
+
+
+def decrypt(iteration_count, message, m, n):
+    res = message
+    # print(m)
+    # print(n)
+    for i in range(iteration_count):
+        res = decrypt_message(res, m[i], n[i])
+        # print(res)
+    return res
 
 
 def init():
     print("Лабораторная работа №4")
-    mes = "I would like to read some characters from a string s1 and put it into another string s2"
-    key = [2, 0, 1]
-    res = crypt(mes, key)
-    print(res)
-    decrypt(res)
+    print("Введите строку для шифрования")
+    f = open("originalText.txt", 'r')
+    mes = f.read()
+    m = [2, 1, 1]
+    n = [4, 3, 2]
+    result = crypt(3, mes, m, n)
+    # decode = decrypt_message(result, 2, 4)
+    # print(decode)
+    m.reverse()
+    n.reverse()
+    decode = decrypt(3, result, m, n)
+    print("Защифрованный текст")
+    print(result)
+    file = open('codeText.txt', 'w')
+    file.write(result)
+    file.close()
+    print("Дешифрованный текст")
+    print(decode)
+    file = open('decodeText.txt', 'w')
+    file.write(decode)
+    file.close()
