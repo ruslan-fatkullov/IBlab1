210176e50a17cbc51815954992735d7de556824c
ruslan-fatkullov <fatkullov1999@gmail.com>
2022-01-08 13:46:20 +0400

    save

--- second_lab.py
+++ second_lab.py
@@ -14,15 +14,16 @@ def init():
     print("Вторая лабораторная работа")
     result_list_of_numbers = list()
     temp = int(time.time())
-    for _ in range(10):
+    for _ in range(100):
         temp = generic_symbol(temp)
         result_list_of_numbers.append(format(temp, 'b'))
     result_string = ""
     list_of_chars = list(result_string.join(result_list_of_numbers))
     print(result_list_of_numbers)
     print(list_of_chars)
-    file2: TextIO = open("second.txt", "w")
-    file2.write(result_string.join(result_list_of_numbers))
+    ert = "".join(list_of_chars)
+    file2 = open("source/second.txt", "w")
+    file2.write(ert)
     file2.close()
     print("Последовательность случайна") if first_lab.frequency_test(list_of_chars) else print("Последовательность НЕ "
                                                                                             "случайна")
--- third_lab.py
+++ third_lab.py
@@ -2,5 +2,8 @@ import hashlib
 
 def init():
     print("Лабораторная работа №3")
-    hash_object = hashlib.md5(b'hELLO WORLD')
+    file = open("source/second.txt", "r")
+    data = file.read()
+    print(data)
+    hash_object = hashlib.md5(data)
     print(hash_object.hexdigest())
